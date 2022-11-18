3f3fc017a47187be101120db81f86f765380cfb4
Alex Christensen <achri19@users.noreply.github.com>
2022-11-17 16:32:37 -0800

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -5,7 +5,7 @@ cd /content
 
 echo "(1) Install pip packages"
 echo "nose mpi4py triangle Pmw pymetis cmocean geopandas fiona pygeos rasterio rasterstats"
-pip install nose mpi4py triangle Pmw pymetis cmocean geopandas fiona pygeos rasterio rasterstats git+https://github.com/simard-landscape-lab/orinoco.git > /dev/null 2>&1
+pip install nose mpi4py triangle Pmw pymetis cmocean geopandas fiona pygeos rasterio rasterstats git+https://github.com/simard-landscape-lab/orinoco.git 
 
 echo "(2) Install gdal"
 apt-get -q -y install python-gdal gdal-bin  > /dev/null 2>&1
