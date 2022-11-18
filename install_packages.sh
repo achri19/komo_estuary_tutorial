70aece3366f683e690b511f7dcbccdedbb4acf38
Alex Christensen <achri19@users.noreply.github.com>
2022-11-17 16:28:34 -0800

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -27,8 +27,9 @@ cd ../
 
 echo "(6) Download Orinoco from github repo"
 echo "https://github.com/simard-landscape-lab/orinoco"
-git clone https://github.com/simard-landscape-lab/orinoco.git 
+git clone https://github.com/simard-landscape-lab/orinoco.git > /dev/null 2>&1
 cd orinoco
-pip install .
+python setup.py --quiet build  > /dev/null 2>&1 
+python setup.py --quiet install  > /dev/null 2>&1
 
 echo "(7) Completed"
