9def073d5e72df96cc6b640c2d1b039b549d4eab
Alex Christensen <achri19@users.noreply.github.com>
2022-11-17 16:31:38 -0800

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -5,7 +5,7 @@ cd /content
 
 echo "(1) Install pip packages"
 echo "nose mpi4py triangle Pmw pymetis cmocean geopandas fiona pygeos rasterio rasterstats"
-pip install nose mpi4py triangle Pmw pymetis cmocean geopandas fiona pygeos rasterio rasterstats > /dev/null 2>&1
+pip install nose mpi4py triangle Pmw pymetis cmocean geopandas fiona pygeos rasterio rasterstats git+https://github.com/simard-landscape-lab/orinoco.git > /dev/null 2>&1
 
 echo "(2) Install gdal"
 apt-get -q -y install python-gdal gdal-bin  > /dev/null 2>&1
@@ -24,12 +24,5 @@ python setup.py --quiet build  > /dev/null 2>&1
 python setup.py --quiet install  > /dev/null 2>&1
 
 cd ../
-
-echo "(6) Download Orinoco from github repo"
-echo "https://github.com/simard-landscape-lab/orinoco"
-git clone https://github.com/simard-landscape-lab/orinoco.git 
-cd orinoco
-python setup.py build 
-python setup.py install  
 
 echo "(7) Completed"
